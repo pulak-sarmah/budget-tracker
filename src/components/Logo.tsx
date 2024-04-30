dd40100023a6a45dd90eac2113b6ed35caa21823
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-30 20:34:25 +0530

    Add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import { PiggyBank } from "lucide-react";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className=" font-bold text-2xl flex gap-1 items-center">
+      <PiggyBank className="text-fuchsia-600" />
+      <span className="text-2xl bg-gradient-to-r from-fuchsia-600 to-pink-600 bg-clip-text text-transparent">
+        Budget Tracker
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
