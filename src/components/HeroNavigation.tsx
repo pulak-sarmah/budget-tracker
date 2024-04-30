28332874c53e9a00d28c4bcf636ae6b22b3efb16
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-30 20:35:45 +0530

    Add dashboard page component

--- src/components/HeroNavigation.tsx
+++ src/components/HeroNavigation.tsx
@@ -2,6 +2,21 @@
 import Link from "next/link";
 import { useUser } from "@clerk/clerk-react";
 
+const NavLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
+  <Link
+    className="inline-flex h-10 items-center justify-center rounded-md bg-slate-900 px-8 text-sm font-medium text-gray-50  transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 animate-accordion-down shadow-2xl"
+    href={href}
+  >
+    {children}
+  </Link>
+);
+
 const HeroNavigation = () => {
   const { user } = useUser();
 
@@ -9,26 +24,11 @@ const HeroNavigation = () => {
     <div className="flex flex-col gap-2 min-[400px]:flex-row">
       {!user ? (
         <>
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-slate-900 px-8 text-sm font-medium text-gray-50  transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 animate-accordion-down shadow-2xl"
-            href="/sign-in"
-          >
-            Sign In
-          </Link>
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-slate-900 px-8 text-sm font-medium text-gray-50  transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 animate-accordion-down shadow-2xl"
-            href="/sign-up"
-          >
-            Sign Up
-          </Link>
+          <NavLink href="/sign-in">Sign In</NavLink>
+          <NavLink href="/sign-up">Sign Up</NavLink>
         </>
       ) : (
-        <Link
-          className="inline-flex h-10 items-center justify-center rounded-md bg-slate-900 px-8 text-sm font-medium text-gray-50  transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 animate-accordion-down shadow-2xl"
-          href="/sign-up"
-        >
-          Go to Dashboard
-        </Link>
+        <NavLink href="/dashboard">Go to Dashboard</NavLink>
       )}
     </div>
   );
