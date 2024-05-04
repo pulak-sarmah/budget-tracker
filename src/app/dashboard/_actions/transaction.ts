fbbf22cdcad30cd06925cec362219d5beb2db324
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-04 12:45:19 +0530

    feat: Add createTransaction function for creating new transactions

---
+++ src/app/dashboard/_actions/transaction.ts
@@ -0,0 +1,102 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+import {
+  createTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transaction";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function createTransaction(form: CreateTransactionSchemaType) {
+  const parsedBody = createTransactionSchema.safeParse(form);
+
+  if (!parsedBody.success) {
+    throw new Error(parsedBody.error.message);
+  }
+
+  const user = await currentUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const { amount, category, date, description, type } = parsedBody.data;
+  const categoryRow = await prisma.category.findFirst({
+    where: {
+      userId: user.id,
+      name: category,
+    },
+  });
+  if (!categoryRow) {
+    throw new Error("Category not found");
+  }
+
+  await prisma.$transaction([
+    prisma.transaction.create({
+      data: {
+        userId: user.id,
+        amount,
+        date,
+        type,
+        description: description || "",
+        category: categoryRow.name,
+        categoryIcon: categoryRow.icon,
+      },
+    }),
+    // Update the category table with the new transaction amount for the month
+    prisma.monthHistory.upsert({
+      where: {
+        userId_day_month_year: {
+          userId: user.id,
+          day: date.getUTCDate(),
+          month: date.getUTCMonth(),
+          year: date.getUTCFullYear(),
+        },
+      },
+      create: {
+        userId: user.id,
+        day: date.getUTCDate(),
+        month: date.getUTCMonth(),
+        year: date.getUTCFullYear(),
+        expense: type === "expense" ? amount : 0,
+        income: type === "income" ? amount : 0,
+      },
+      update: {
+        expense: {
+          increment: type === "expense" ? amount : 0,
+        },
+        income: {
+          increment: type === "income" ? amount : 0,
+        },
+      },
+    }),
+
+    // Update the category table with the new transaction amount for the year
+    prisma.yearHistory.upsert({
+      where: {
+        userId_month_year: {
+          userId: user.id,
+          month: date.getUTCMonth(),
+          year: date.getUTCFullYear(),
+        },
+      },
+      create: {
+        userId: user.id,
+        month: date.getUTCMonth(),
+        year: date.getUTCFullYear(),
+        expense: type === "expense" ? amount : 0,
+        income: type === "income" ? amount : 0,
+      },
+      update: {
+        expense: {
+          increment: type === "expense" ? amount : 0,
+        },
+        income: {
+          increment: type === "income" ? amount : 0,
+        },
+      },
+    }),
+  ]);
+}
