ef60f3f949190b9441fcd476d6c7a6e50149aae2
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-05 21:08:01 +0530

    feat: Update currency values and labels for various currencies

--- src/lib/currencies.ts
+++ src/lib/currencies.ts
@@ -1,33 +1,103 @@
 export const currencies = [
   {
-    value: "USD",
-    label: "$ Dollar",
-    locale: "en-US",
+    value: "AUD",
+    label: "A$ Dollar (🇦🇺)",
+    locale: "en-AU",
   },
   {
-    value: "RUPEE",
-    label: "₹ Rupee",
-    locale: "en-IN",
+    value: "BRL",
+    label: "R$ Real (🇧🇷)",
+    locale: "pt-BR",
   },
   {
-    value: "YEN",
-    label: "¥ Yen",
-    locale: "ja-JP",
+    value: "CAD",
+    label: "CA$ Dollar (🇨🇦)",
+    locale: "en-CA",
   },
   {
-    value: "EURO",
-    label: "€ Euro",
+    value: "CHF",
+    label: "CHF Franc (🇨🇭)",
+    locale: "de-CH",
+  },
+  {
+    value: "CNY",
+    label: "¥ Yuan (🇨🇳)",
+    locale: "zh-CN",
+  },
+  {
+    value: "EUR",
+    label: "€ Euro (🇪🇸)",
     locale: "de-DE",
   },
   {
-    value: "POUND",
-    label: "£ Pound",
+    value: "GBP",
+    label: "£ Pound (🇬🇧)",
     locale: "en-GB",
   },
   {
-    value: "RUBLE",
-    label: "₽ Ruble",
+    value: "HKD",
+    label: "HK$ Dollar (🇭🇰)",
+    locale: "zh-HK",
+  },
+  {
+    value: "INR",
+    label: "₹ Rupee (🇮🇳)",
+    locale: "en-IN",
+  },
+  {
+    value: "JPY",
+    label: "¥ Yen (🇯🇵)",
+    locale: "ja-JP",
+  },
+  {
+    value: "KRW",
+    label: "₩ Won (🇰🇷)",
+    locale: "ko-KR",
+  },
+  {
+    value: "MXN",
+    label: "Mex$ Peso (🇲🇽)",
+    locale: "es-MX",
+  },
+  {
+    value: "NOK",
+    label: "kr Krone (🇳🇴)",
+    locale: "no-NO",
+  },
+  {
+    value: "NZD",
+    label: "NZ$ Dollar (🇳🇿)",
+    locale: "en-NZ",
+  },
+  {
+    value: "RUB",
+    label: "₽ Ruble (🇷🇺)",
     locale: "ru-RU",
+  },
+  {
+    value: "SEK",
+    label: "kr Krona (🇸🇪)",
+    locale: "sv-SE",
+  },
+  {
+    value: "SGD",
+    label: "S$ Dollar (🇸🇬)",
+    locale: "en-SG",
+  },
+  {
+    value: "TRY",
+    label: "₺ Lira (🇹🇷)",
+    locale: "tr-TR",
+  },
+  {
+    value: "USD",
+    label: "$ Dollar (🇺🇸)",
+    locale: "en-US",
+  },
+  {
+    value: "ZAR",
+    label: "R Rand (🇿🇦)",
+    locale: "en-ZA",
   },
 ];
 
