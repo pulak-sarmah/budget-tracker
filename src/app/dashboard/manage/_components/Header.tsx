98415a518ccbb0f71a59792eaa0bcc84e2c25204
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-13 10:34:59 +0530

    feat: Add Header component for managing dashboard

---
+++ src/app/dashboard/manage/_components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="border-b bg-card">
+      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div>
+          <p className="text-3xl font-bold">Manage</p>
+          <p className="text-muted-foreground">
+            Manage your account settings and categories
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
