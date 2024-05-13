c8166591ad2feca17375c8b354c08498e450b5ac
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-13 10:34:38 +0530

    feat: Add DeleteCategorySchema for deleting categories
    - Add DeleteCategorySchema to define the schema for deleting categories, including the required properties "name" (string, min length 3, max length 20) and "type" (enum: "income" or "expense").

--- src/schema/categories.ts
+++ src/schema/categories.ts
@@ -7,3 +7,10 @@ export const CreateCtegorySchema = z.object({
 });
 
 export type CreateCategorySchemaType = z.infer<typeof CreateCtegorySchema>;
+
+export const DeleteCategorySchema = z.object({
+  name: z.string().min(3).max(20),
+  type: z.enum(["income", "expense"]),
+});
+
+export type DeleteCategorySchemaType = z.infer<typeof DeleteCategorySchema>;
